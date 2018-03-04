409b60bde1098ede6f27eef10e290a5adf3a0431
Michael Panik <michael@plexamedia.com>
2018-03-04 16:46:42 -0600

    Add live, AJAX searching to get unique film ID and other info.

--- js/film-search.js
+++ js/film-search.js
@@ -1,17 +1,23 @@
+//TODO: I think the liveSearch() is handling the selection poorly. Needs to be
+// its own function, probably do some other cool stuff when it's called.
+
 function liveSearch() {
   let input = event.target.value
 
-  const dataList = $('datalist#films')
+  const $dataList = $('datalist#films')
 
-  if(input.length >= 5) {
-    html = [...queryFilms(input)].map(x => `<option value="${x.Title}" data-id="${x.imdbID}">`)
-    dataList.html(html.join(''))
+  if(input.length >= 3) {
+    html = [...queryFilms(input)].map(x => `<option value="${x.title}" data-id="${x.id}">`)
+    $dataList.html(html.join(''))
   }
 
-  var option = dataList.find("[value='" + input + "']");
+  let option = $dataList.find("[value='" + input + "']")
 
-  if (option.length > 0) {
-    $("#filmID").val(option.data("id"))
+  if(option.length > 0) {
+    var id = option.data('id')
+    $('input#filmID').val(id)
+    
+    $dataList.html('')
   }
 }
 
@@ -21,12 +27,12 @@ function queryFilms(query) {
   let results = []
 
   $.ajax({
-      url: `http://www.omdbapi.com/?apikey=b33c94bf&s=${query}`,
+      url: `https://api.themoviedb.org/3/search/movie?api_key=20335e49e1179cf9d833f158009d13d9&language=en-US&query=${query}&page=1&include_adult=false`,
       type: 'get',
       dataType: 'JSON',
       async: false,
       success: function(data){
-        results = data.Search
+        results = data.results
       }
   })
 
@@ -34,5 +40,29 @@ function queryFilms(query) {
     return results
   } else {
     return []
+  }
+}
+
+function getFilmDetails(film_id) {
+  let filmDetails = {}
+
+  $.ajax({
+      url: `https://api.themoviedb.org/3/movie/${film_id}?api_key=20335e49e1179cf9d833f158009d13d9&language=en-US`,
+      type: 'get',
+      dataType: 'JSON',
+      async: false,
+      success: function(data){
+        filmDetails = {
+          title: data.results.title,
+          release_date: `http://image.tmdb.org/t/p/w500/${data.results.release_date}`,
+          poster: data.results.poster_path,
+        }
+      }
+  })
+
+  if (filmDetails.length > 0) {
+    return filmDetails
+  } else {
+    return {}
   }
 }
